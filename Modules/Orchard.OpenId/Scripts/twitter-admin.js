8230def254d449f2ad030bd9b596c2fcfffb63de
Michael Washam <michael@opsgility.com>
2024-01-30 10:35:53 -0500

    updated version

--- Modules/Orchard.OpenId/Scripts/twitter-admin.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).on('click', '#colExpControl', function () {
-    if ($('#colExpArea').css('display') == 'none') {
-        $('#colExpArea').show(300);
-        $('#colExpButton').html('-');
-    } else {
-        $('#colExpArea').hide(300);
-        $('#colExpButton').html('+');
-    }
-});
